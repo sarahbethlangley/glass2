319ae375cc0ee5d052e6281bb7ea360a6c909787
Sarah Beth Langley <sarahbethdoescode@gmail.com>
2023-03-22 13:52:48 -0500

    done

---
+++ main.js
@@ -0,0 +1,32 @@
+const dishes = [
+  "Dinner plate",
+  "Water glass",
+  "Salad bowl",
+  "Dinner plate",
+  "Wine glass",
+  "Spoon",
+  "Spoon",
+  "Fork",
+  "Salad bowl",
+  "Whiskey glass",
+  "Fork",
+  "Spoon",
+  "Fork",
+];
+
+/*
+    Declare three variables to store the string value
+    of a glass in the array. Use the correct index to
+    get the right value.
+*/
+
+const water = dishes[1];
+const wine = dishes[4];
+const whiskey = dishes[9];
+
+console.log("I am cleaning the following glasses:");
+
+// Put each variable in one of the parenthesis below
+console.log(water);
+console.log(wine);
+console.log(whiskey);
